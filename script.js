a8581fb1d430b41c3290a2d7057272983a658bfa
pabitra1956 <samalpabitra91@gmail.com>
2025-11-16 15:53:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 function getWeather() {
     const city = document.getElementById("city-input").value;
     const apiKey = "e3e3bc6382004fbeebce520ec644a60e";   // Replace with your API key
-
     if(city === "") {
         alert("Please enter a city name!");
         return;
     }
-
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -16,11 +13,10 @@ function getWeather() {
                 document.getElementById("result").innerHTML = "<p>City not found!</p>";
                 return;
             }
-
             const html = `
                 <h3>${data.name}</h3>
                 <p><strong>Temperature:</strong> ${data.main.temp} °C</p>
-                <p><strong>Humidity:</strong> ${data.main.humidity}%</p>
+                <p><strong>Humidity:</strong> ${data.main.humidity} %</p>
                 <p><strong>Weather:</strong> ${data.weather[0].description}</p>
                 <p><strong>WindSpeed:</strong> ${data.wind.speed} m/s</p>
             `;
