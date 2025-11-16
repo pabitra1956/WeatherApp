b4392b2f6b3ac4b96559b5fdacea9abdd3b9a95b
pabitra1956 <samalpabitra91@gmail.com>
2025-11-16 13:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getWeather() {
         return;
     }
 
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q= ('${city}') &appid= ('${apiKey}') &units=metric';
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q= '${city}'&appid='${apiKey}'&units=metric';
 
     fetch(url)
         .then(response => response.json())
